4e65d0b0c3243f7728a08433d2e521fd834a86b3
DESKTOP-1IPN0H6\asusrogandrii <0999garasim@gmail.com>
2023-10-13 21:04:10 +0300

    Add global exception catcher.

--- FlexWords.Dialog/App.xaml.cs
+++ FlexWords.Dialog/App.xaml.cs
@@ -1,6 +1,37 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+using Newtonsoft.Json;
 
 namespace FlexWords.Dialog
 {
-    public partial class App : Application { }
+    public partial class App : Application
+    {
+        public App()
+        {
+            DispatcherUnhandledException += OnAppUnhandledException;
+        }
+
+        private void OnAppUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string filter = "JSON file (*.json)|*.json";
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            var ofd = new SaveFileDialog
+            {
+                Filter = filter,
+                InitialDirectory = folder
+            };
+
+            if (ofd.ShowDialog() ?? false)
+            {
+                Exception exception = e.Exception;
+                File.WriteAllText(ofd.FileName, JsonConvert.SerializeObject(exception));
+            }
+
+            e.Handled = true;
+        }
+    }
 }
